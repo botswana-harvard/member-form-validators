8cab75b1c00f15ad0c43211deb7560e158c4eed7
Gofaone Magodi <magodign@magodign.local>
2017-10-01 15:59:30 +0200

    fix member validations

--- member_form_validators/form_validators/moved_member.py
+++ member_form_validators/form_validators/moved_member.py
@@ -43,49 +43,7 @@ class MovedMemberFormValidator(FormValidator):
             field_applicable='personal_details_changed'
         )
 
-        self.not_applicable_if(
-            NO,
-            field='present_today',
-            field_applicable='inability_to_participate'
-        )
-
-        self.not_applicable_if(
-            NO,
-            field='present_today',
-            field_applicable='study_resident'
-        )
-
-        self.not_applicable_if(
-            NO,
-            field='present_today',
-            field_applicable='personal_details_changed'
-        )
-
-        self.not_applicable_if(
-            NO,
-            field='present_today',
-            field_applicable='details_change_reason'
-        )
-
-        self.not_applicable_if(
-            YES,
-            field='has_moved',
-            field_applicable='inability_to_participate'
-        )
-
-        self.not_applicable_if(
-            YES,
-            field='has_moved',
-            field_applicable='study_resident'
-        )
-
-        self.not_applicable_if(
-            YES,
-            field='has_moved',
-            field_applicable='personal_details_changed'
-        )
-
-        if self.instance.id and self.instance.has_moved not in [YES] and self.instance.present_today not in [NO]:
+        if self.instance.id and self.instance.has_moved not in [YES]:
             raise forms.ValidationError(
                 'Household Member does not indicate that this member has moved',
                 code='household_member_has_moved')
